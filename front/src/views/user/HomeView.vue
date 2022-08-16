00df68da38a9cd6f54f9f2714be77420ff1739ad
THEAVY.VUN <THEAVY.VUN@pnc.lan>
2022-08-16 19:06:50 +0700

    #9 request history front

--- front/src/views/user/HomeView.vue
+++ front/src/views/user/HomeView.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="home bg-[#ddd] w-full h-[96vh] ">
-    <div class="card w-3/5 m-auto   rounded-xl p-10 shadow- bg-[white]">
+  <div class="home bg-[#ddd] w-full h-[90vh] pt-16">
+    <div class="card w-8/12 m-auto rounded-xl p-10 shadow- bg-[white]">
       <div class="card-header">
-        <h1 class="text-4xl m-5 font-bold ">Student Leave Management </h1>
-        <h1 class="text-4xl m-5 font-bold ">System (SLMS) </h1>
+        <h1 class="text-4xl m-5 font-bold ">Student Leave Management System (SLMS)</h1>
       </div>
       <div class="card-body pl-3.5">
-        <p>
+        <p class="text-[20px]">
           It is a software that streamlines time off requests for students. Typically managed by social affair team, an
           automated leave management system makes it easy to track, approve, or deny student leave requests.
           Students use their school email address and password to log in. When they log in, they have access to their
