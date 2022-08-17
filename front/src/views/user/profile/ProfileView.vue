9868ebbe805fc03c988d6cbb4cfd5e469da056c3
Huymey.KUM <Huymey.KUM@pnc.lan>
2022-08-17 10:55:35 +0700

    #12 user notification

--- front/src/views/user/profile/ProfileView.vue
+++ front/src/views/user/profile/ProfileView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="home bg-[#ddd] w-full h-[110vh] pt-16">
+    <div class="home bg-[#ddd] w-full h-[110vh] pt-[100px]">
         <div class="card w-8/12 m-auto rounded-xl shadow- bg-[white]">
             <div class="card-header bg-[#0081CA] p-2 text-center text-[30px] text-white">
                 <h1>My Profile</h1>
@@ -10,6 +10,10 @@
                         stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 bg-[#ddd]  rounded-full px-1 absolute ml-[50px] mt-[50px]" viewBox="0 0 20 20" fill="currentColor">
+                        <path
+                            d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                     </svg>
                     <div class="p-5">
                         <div>
@@ -40,13 +44,13 @@
                         <div class="reset-password pt-10">
                             <button class="bg-[#FF0000] p-1.5 rounded-md text-white flex">
                                 <router-link class="p-2 px-6 flex" to="/newRequest">
-                                Reset Password
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 " viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                        d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z"
-                                        clip-rule="evenodd" />
-                                </svg>
+                                    Reset Password
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 " viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z"
+                                            clip-rule="evenodd" />
+                                    </svg>
                                 </router-link>
 
                             </button>
