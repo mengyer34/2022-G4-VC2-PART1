f6a3f59b8b3f6accf6d8e2fc7ff2578a9ba61b25
SEYHA.SOEURN <SEYHA.SOEURN@pnc.lan>
2022-08-17 10:44:28 +0700

    #7 Crud leaves back-end

--- back/app/Http/Controllers/LeaveController.php
+++ back/app/Http/Controllers/LeaveController.php
@@ -14,7 +14,17 @@ class LeaveController extends Controller
      */
     public function index()
     {
-        //
+        return Leave::all();
+    }
+
+    /**
+     * Display a listing of the resource with user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLeavesUser()
+    {
+        return Leave::with('user')->get();
     }
 
     /**
@@ -25,18 +35,40 @@ class LeaveController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $leave = new Leave();
+        $leave->user_id = $request->user_id;
+        $leave->start_date = $request->start_date;
+        $leave->end_date = $request->end_date;
+        $leave->duration = $request->duration;
+        $leave->leave_type = $request->leave_type;
+        $leave->reason = $request->reason;
+        $leave->status = 'Pending';
+        $leave->is_review = false;
+        $leave->save();
+
+        return response()->json(['message'=>'Leave created successfully']);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Leave  $leave
+     * @param  \App\Models\Leave  $id
      * @return \Illuminate\Http\Response
      */
     public function show(Leave $leave)
     {
-        //
+        return $leave;
+    }
+
+    /**
+     * Display the specified resource with user.
+     *
+     * @param  \App\Models\Leave  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function getLeaveUser($id)
+    {
+        return Leave::with('user')->findOrFail($id);
     }
 
     /**
@@ -46,9 +78,26 @@ class LeaveController extends Controller
      * @param  \App\Models\Leave  $leave
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Leave $leave)
+    public function updateStatus(Request $request, Leave $leave)
     {
-        //
+        $leave->status = $request->status;
+        $leave->save();
+
+        return response()->json(['message'=>'Leave updated successfully']);
+    }
+
+    /** Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Models\Leave  $leave
+    * @return \Illuminate\Http\Response
+    */
+    public function updateAsViewed(Request $request, Leave $leave)
+    {
+        $leave->is_review = true;
+        $leave->save();
+
+        return response()->json(['message'=>'Leave updated successfully']);
     }
 
     /**
@@ -59,6 +108,6 @@ class LeaveController extends Controller
      */
     public function destroy(Leave $leave)
     {
-        //
+        return $leave->delete();
     }
 }
--- back/app/Models/Leave.php
+++ back/app/Models/Leave.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Leave extends Model
 {
     use HasFactory;
+
+    // Change from number (1, 0) to (true, false)
+    protected $casts = [
+        'is_review' => 'boolean',
+    ];
+
+    // Relationship
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
