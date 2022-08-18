f7252f294b1fde4de7079c4ee252601402f33c8f
THEAVY.VUN <THEAVY.VUN@pnc.lan>
2022-08-18 08:15:11 +0700

    update leaves request

--- front/src/components/UserLeaveHistory.vue
+++ front/src/components/UserLeaveHistory.vue
@@ -1,34 +1,124 @@
 <template>
-    <div class="w-9/12 m-auto bg-slate-300 rounded-md mt-3 p-8 shadow-sm shadow-gray-600">
-        <p>Total: {{leaves.length}}</p>
-        <leave-card v-for="leave of leavesstatus" :key="leave" :leave="leave"/>
+    <div class="bg-white shadow md:px-5 pt-4 md:pt-7 pb-5 overflow-y-auto">
+        <table class="w-full whitespace-nowrap">
+            <thead>
+                <tr tabindex="0" class="focus:outline-none h-16 w-full text-sm leading-none text-gray-800 border-b-2 border-primary">
+                    <th class="font-normal text-left pl-4">Reason</th>
+                    <th class="font-normal text-left pl-12">Leave type</th>
+                    <th class="font-normal text-left pl-12">Start Date</th>
+                    <th class="font-normal text-left pl-20">End Date</th>
+                    <th class="font-normal text-left pl-20">Duration</th>
+                    <th class="font-normal text-left pl-16">Status</th>
+                    <th class="font-normal text-left pl-16">Request Date</th>
+                </tr>
+            </thead>
+            <tbody v-if="leavesstatus.length > 0" class="w-full">
+                <tr  v-for="leave of leavesstatus" :key="leave" tabindex="0" class="focus:outline-none h-20 text-sm leading-none text-gray-800 bg-white hover:bg-gray-200 border-b border-t border-gray-100">
+                    <td class="pl-4 cursor-pointer">
+                        <div class="flex items-center">
+                            <p class="font-medium">{{leave.reason}}</p>
+                        </div>
+                    </td>
+                    <td class="pl-12">
+                        {{leave.leave_type}}
+                    </td>
+                    <td class="pl-12">
+                        {{date_format(leave.start_date)}} ({{isStartAt(leave.start_date, leave.end_date, leave.duration)}})
+                    </td>
+                    <td class="pl-20">
+                        {{date_format(leave.end_date)}} ({{isEndAt(leave.start_date, leave.end_date, leave.duration)}})
+                    </td>
+                    <td class="pl-20">
+                        <p class="font-medium">{{leave.duration}}</p>
+                    </td>
+                    <td class="pl-16">
+                        <div class="flex items-center">
+                            <span class="px-4 py-2 rounded-full text-white" :class="iscolor(leave)">{{leave.status}}</span>
+                        </div>
+                    </td>
+                    <td class="pl-16">
+                        <div class="flex items-center">
+                            <span>{{format_date(leave.created_at)}}</span>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+            <tbody v-else>
+                <tr>
+                    <td colspan="7" class="p-2 text-center">No leaves Found!!</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
+    <h1></h1>
 </template>
 
 <script>
-import UserLeaveCard from './UserLeaveCard.vue'
+    import moment from 'moment'
 
-export default {
-    components: {
-      'leave-card': UserLeaveCard,
-    },
-    props: ['leaves', 'status'],
-    data() {
-      return {
-      };
-    },
-    computed: {
-        leavesstatus() {
-            if (this.status == "Approve") {
-                return this.leaves.filter(leave => leave.status == "Approve");
-            }else if (this.status == "Panding") {
-                return this.leaves.filter(leave => leave.status == "Panding");
-            }else if (this.status == "Reject") {
-                return this.leaves.filter(leave => leave.status == "Reject");
-            }else {
-                return this.leaves;
+    export default {
+        props: ['leaves', 'status', 'type'],
+        data() {
+            return {
+
+            };
+        },
+        computed: {
+            leavesstatus() {
+                if (this.status != "All" && this.type == "All") {
+                    return this.leaves.filter(leave => leave.status == this.status);
+                }else if (this.status == "All" && this.type != "All") {
+                    return this.leaves.filter(leave => leave.leave_type == this.type);
+                }else if (this.status != "All" && this.type != "All") {
+                    return this.leaves.filter(leave => leave.status == this.status && leave.leave_type == this.type);
+                }else {
+                    return this.leaves;
+                }
+            },
+
+        },
+        methods: {
+            iscolor(leave) {
+                if (leave.status == "Approved") {
+                    return "bg-green-500";
+                }else if (leave.status == "Pending") {
+                    return "bg-orange-500";
+                }else if (leave.status == "Reject") {
+                    return "bg-red-500";
+                }
+            },
+            format_date(value){
+                if (value) {
+                    return moment(String(value)).format('MMMM DD, YYYY hh:mm A')
+                }
+            },
+            date_format(value){
+                if (value) {
+                    return moment(String(value)).format('MMMM DD, YYYY')
+                }
+            },
+            parseDate(start, end) {
+                var date1 = new Date(start);
+                var date2 = new Date(end);
+                var diffDays = parseInt((date2 - date1) / (1000 * 60 * 60 * 24), 10); 
+                return diffDays;
+            },
+            isStartAt(start, end, duration) {
+                let day = this.parseDate(start, end);
+                if ( day < duration) {
+                    return "Morning";
+                }else if (day > duration) {
+                    return "Afternoon";
+                }
+            },
+            isEndAt(start, end, duration) {
+                let day = this.parseDate(start, end);
+                if ( day < duration) {
+                    return "Afternoon";
+                }else if (day > duration) {
+                    return "Morning";
+                }
             }
-        }
+        },
     }
-}
 </script>
--- front/src/views/user/history/HistoryView.vue
+++ front/src/views/user/history/HistoryView.vue
@@ -1,31 +1,59 @@
 <template>
-    <div class="mt-28 bg-slate-300 w-9/12 p-6 m-auto rounded-md flex shadow-gray-600 shadow-sm">
-        <div class="w-4/12">
-            <label for="filter-status"><span class="text-red-600">*</span>Filter by status:</label><br>
-            <select v-model="status" name="" id="filter-status" class="w-9/12 rounded p-2">
-                <option value="All">All</option>
-                <option value="Reject">Reject</option>
-                <option value="Approve">Approve</option>
-                <option value="Panding">Panding</option>
-            </select>
-        </div>
-        <div class="w-4/12">
-            <label for="filter-leave-type"><span class="text-red-600">*</span>Filter by leaves type:</label><br>
-            <select v-model="type" name="" id="filter-leave-type" class="w-9/12 p-2 rounded">
-                <option value="All">All</option>
-                <option value="Family">Family's Event</option>
-                <option value="Sick">Sick</option>
-                <option value="Homesick">Home Sick</option>
-            </select>
-        </div>
-    </div>
-    <leave-history :leaves="leaves" :status="status" />
-    <h1></h1>
+    <div class="w-full sm:px-6 mt-24">
+        <div class="px-4 md:px-10 py-4 md:py-7 bg-gray-200 rounded-tl-lg rounded-tr-lg">
+            <div class="sm:flex items-center justify-between">
+                <p tabindex="0" class="flex items-center focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                        <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" />
+                    </svg>
+                    <span class="ml-1">
+                        List all Leaves
+                    </span>
+                </p>
+                <div>
+                    <button class="focus:ring-2 focus:ring-offset-2 focus:ring-primary inline-flex sm:ml-3 mt-4 sm:mt-0 items-start justify-start px-3 bg-primary focus:outline-none rounded">
+                    <p class="text-sm font-medium leading-none text-white flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z" clip-rule="evenodd" />
+                        </svg>
+                        <span>
+                            Print All
+                        </span>
+                    </p>
+                    </button>
+                </div>
+            </div>
+            <div class="flex">
+                <div class="w-2/6">
+                    <label for="filter-status text-sm leading-none text-gray-800"><span class="text-red-600">*</span>Filter by status:</label><br>
+                    <select v-model="status" name="" id="filter-status" class="w-9/12 rounded p-1">
+                        <option value="All">All</option>
+                        <option value="Rejected">Rejected</option>
+                        <option value="Approved">Approved</option>
+                        <option value="Cancel">Cancel</option>
+                        <option value="Pending">Pending</option>
+                    </select>
+                </div>
+                <div class="w-2/6">
+                    <label for="filter-leave-type font-normal "><span class="text-red-600">*</span>Filter by leaves type:</label><br>
+                    <select v-model="type" name="" id="filter-leave-type" class="w-9/12 p-1 rounded">
+                        <option value="All">All</option>
+                        <option value="Homesick">Home Sick</option>
+                        <option value="Sick">Sick</option>
+                        <option value="Family's events">Family's Event</option>
+                    </select>
+                </div>
+            </div>
 
+        </div>
+        <leave-history :leaves="leaves" :status="status" :type="type" />
+    </div>
 </template>
 
+
 <script>
-// @ is an alias to /src
+import axios from '../../../axios-http.js'
 import UserLeaveHistory from '../../../components/UserLeaveHistory.vue'
 
 export default {
@@ -36,64 +64,20 @@ export default {
     return {
         status: "All",
         type: "All",
-        leaves: [
-            {
-                id: 1,
-                userId: 1,
-                startDate: "February 21, 2022",
-                endDate: "February 22, 2022",
-                duration: 1,
-                requestedDate: "February 20, 2022 7:30 AM",
-                leaveType: "Family's Event",
-                reason: "Go to hometown sdf dfik sisi sfsd ksdf dkdsjid sfkdfd fesksdfsjd sdlfkd sidk fikd skfjsdf ie sdksfjsse sd ksfdjksdfjks",
-                status: "Panding",
-                isViewed: false
-            },
-            {
-                id: 1,
-                userId: 1,
-                startDate: "February 21, 2022",
-                endDate: "February 22, 2022",
-                duration: 1,
-                requestedDate: "February 20, 2022 7:30 AM",
-                leaveType: "Family's Event",
-                reason: "Go to hometown",
-                status: "Approve",
-                isViewed: false
-            },
-            {
-                id: 1,
-                userId: 1,
-                startDate: "February 21, 2022",
-                endDate: "February 22, 2022",
-                duration: 1,
-                requestedDate: "February 20, 2022 7:30 AM",
-                leaveType: "Family's Event",
-                reason: "Go to hometown",
-                status: "Approve",
-                isViewed: false
-            },
-            {
-                id: 1,
-                userId: 1,
-                startDate: "February 21, 2022",
-                endDate: "February 22, 2022",
-                duration: 1,
-                requestedDate: "February 20, 2022 7:30 AM",
-                leaveType: "Family's Event",
-                reason: "Go to hometown",
-                status: "Reject",
-                isViewed: false
-            },
-
-
-
-         ]
+        leaves: [],
     }
   },
   methods: {
-
+    getLeave() {
+        axios.get('http://localhost:8000/api/leaves').then(res => {
+            console.log(res);
+            this.leaves = res.data;
+        })
+    }
   },
+  mounted() {
+    this.getLeave();
+  }
 
 }
 </script>
