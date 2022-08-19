1f70b69d7f0960267684b490952a91c6bc3622a5
huymey <huymey.kum@student.passerellesnumeriques.org>
2022-08-19 09:21:45 +0700

    #12 convert HTML to PDF file on notification

--- front/src/components/user/RequestImfortmation.vue
+++ front/src/components/user/RequestImfortmation.vue
@@ -21,55 +21,61 @@
               <tbody>
                 <tr class="hover:bg-[#F5F5F5]">
                   <td class="border p-3 text-center text-[13px] text-[#6E6E6E]">Reason</td>
-                  <td class="border  p-3 text-center w-[96%] text-[13px] font-bold">{{datas.reason}}</td>
+                  <td class="border  p-3 text-center w-[96%] text-[13px] font-bold">{{ datas.reason }}</td>
                 </tr>
                 <tr class="hover:bg-[#F5F5F5]">
                   <td class="border text-center p-3 text-[13px] text-[#6E6E6E]">Leave Type</td>
-                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#FF6B00]">{{datas.leave_type}} </td>
+                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#FF6B00]">{{ datas.leave_type }} </td>
                 </tr>
                 <tr class="hover:bg-[#F5F5F5]">
                   <td class="border text-center p-3 text-[13px] text-[#6E6E6E]">Start Date</td>
-                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#6E6E6E]">{{datas.start_date}} {{datas.start_time}}</td>
+                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#6E6E6E]">{{ datas.start_date }}
+                    {{ datas.start_time }}</td>
                 </tr>
                 <tr class="hover:bg-[#F5F5F5]">
                   <td class="border text-center p-3 text-[13px] text-[#6E6E6E]">End Date</td>
-                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#6E6E6E]">{{datas.end_date}} {{datas.end_time}}
+                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#6E6E6E]">{{ datas.end_date }}
+                    {{ datas.end_time }}
                   </td>
                 </tr>
                 <tr class="hover:bg-[#F5F5F5]">
                   <td class="border text-center p-3 text-[13px] text-[#6E6E6E]">Status</td>
-                  <td class="border text-center p-3 w-[96%]  text-[13px]   " :class="datas.status ==='Approved' ? 'text-[#0081CA]': 'text-[#FF0000]'">{{datas.status}} </td>
+                  <td class="border text-center p-3 w-[96%]  text-[13px]   "
+                    :class="datas.status === 'Approved' ? 'text-[#0081CA]' : 'text-[#FF0000]'">{{ datas.status }} </td>
                 </tr>
                 <tr class="hover:bg-[#F5F5F5]">
                   <td class="border text-center p-3 text-[13px] text-[#6E6E6E]">Duration</td>
-                  <td class="border text-center p-3 w-[96%]  text-[13px]  text-[#FF6B00]">{{datas.duration}}</td>
+                  <td class="border text-center p-3 w-[96%]  text-[13px]  text-[#FF6B00]">{{ datas.duration }}</td>
                 </tr>
                 <tr class="hover:bg-[#F5F5F5]">
                   <td class="border text-center p-3 text-[13px] text-[#6E6E6E]">Requestoin</td>
-                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#6E6E6E]">{{datas.created_at}}</td>
+                  <td class="border text-center p-3 w-[96%]  text-[13px] text-[#6E6E6E]">{{ datas.created_at }}</td>
                 </tr>
               </tbody>
             </table>
           </div>
           <!--footer-->
         </div>
-          <div class="flex items-center justify-end p-6 border-0  shadow-lg relative  w-full bg-white outline-none focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 hover:text-white w-10 m-2 hover:bg-[#FF6B00] hover:rounded-full p-2 text-[#0081CA]" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 hover:text-white w-10 m-2 hover:bg-[#FF6B00] hover:rounded-full p-2 text-[#0081CA]" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor" stroke-width="2" @click="exportToPDF">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-            </svg>
-            <button
-              class="t bg-transparent border border-solid bg-[#D9D9D9] hover:bg-[#FF6B00] hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button" v-on:click="toggleModal()">
-              Close
-            </button>
-          </div>
+        <div
+          class="flex items-center justify-end p-6 border-0  shadow-lg relative  w-full bg-white outline-none focus:outline-none">
+          <svg xmlns="http://www.w3.org/2000/svg"
+            class="h-10 hover:text-white w-10 m-2 hover:bg-[#FF6B00] hover:rounded-full p-2 text-[#0081CA]" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg"
+            class="h-10 hover:text-white w-10 m-2 hover:bg-[#FF6B00] hover:rounded-full p-2 text-[#0081CA]" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" @click="exportToPDF">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+          </svg>
+          <button
+            class="t bg-transparent border border-solid bg-[#D9D9D9] hover:bg-[#FF6B00] hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button" v-on:click="toggleModal()">
+            Close
+          </button>
+        </div>
       </div>
     </div>
     <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
@@ -80,8 +86,8 @@
 import html2pdf from "html2pdf.js";
 export default {
   name: "regular-modal",
-  props:{
-    'datas':Object
+  props: {
+    'datas': Object
 
   },
   data() {
@@ -90,13 +96,13 @@ export default {
     }
   },
   methods: {
-    toggleModal: function(){
+    toggleModal: function () {
       this.showModal = !this.showModal;
     },
     exportToPDF() {
       html2pdf(document.getElementById("element-to-convert"),
         {
-          
+
           margin: 5,
           filename: "imformation.pdf",
         });
--- front/src/views/user/notification/NotificationView.vue
+++ front/src/views/user/notification/NotificationView.vue
@@ -1,41 +1,28 @@
 <template>
 
     <div class="home bg-[#ddd] w-full h-[90vh] pt-[100px]">
-        <div v-for="data of dataes" :key="data" class="card w-8/12 m-auto rounded-l p-3  shadow- bg-[white]">
-            <div v-if="data.status === 'Approved'" class="pr-3 pl-3">
+        <div v-for="data of dataOfImformation" :key="data" class="card w-8/12 m-auto  p-3  shadow- bg-[white]">
+            <div class="pr-3 pl-3">
                 <imformation-requestion :datas="data">
                     <template #allow>
                         <div class="bg-[#F5F5F5] rounded-l  flex justify-between ">
                             <div class="p-2">
-                                <p class="text-[22px]">You are allowed to <span class="text-[#7BE77B]">go to
+                                <p class="text-[22px]">You are allowed to <span
+                                        :class="data.status === 'Approved' ? 'text-[#7BE77B]' : 'text-[#FF0000]'">go to
                                         homework!</span>
                                 </p>
-                                <p class="text-[12px] text-[#AAAAAA] flex justify-start">February 20, 2018 7:30AM</p>
+                                <p class="text-[12px] text-[#AAAAAA] flex justify-start">{{ data.updated_at }}</p>
                             </div>
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="h-10 w-10 m-3 bg-[#7BE77B]  rounded-full px-1 text-white" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                                v-if="data.status === 'Approved'">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
                             </svg>
-                        </div>
-                    </template>
-                </imformation-requestion>
-                <p class="text-[12px] text-[#AAAAAA] flex justify-end">From: Socail Affair</p>
-            </div>
-
-            <div v-if="data.status === 'Rejected'" class="pr-3 pl-3">
-                <imformation-requestion :datas="data">
-                    <template #allow>
-                        <div class="bg-[#F5F5F5] rounded-l flex justify-between">
-                            <div class="p-2">
-                                <p class="text-[22px]">You are allowed to <span class="text-[#FF0000]">go to
-                                        homework!</span>
-                                </p>
-                                <p class="text-[12px] text-[#AAAAAA] flex justify-start">From: Socail Affair</p>
-                            </div>
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="h-10 w-10 bg-[#FF0000]  rounded-full m-3 p-1 text-white" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                                v-if="data.status === 'Rejected'">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </div>
@@ -43,7 +30,6 @@
                 </imformation-requestion>
                 <p class="text-[12px] text-[#AAAAAA] flex justify-end">From: Socail Affair</p>
             </div>
-
         </div>
     </div>
     <h1></h1>
@@ -72,9 +58,8 @@ export default {
 
     },
     computed: {
-        dataes() {
+        dataOfImformation() {
             return this.datas.filter(data => data.status != "Pending");
-            // }
         },
 
     },
