9557c5cdd7c942706aeff2bd2659db12cbc92427
SEYHA.SOEURN <SEYHA.SOEURN@pnc.lan>
2022-08-21 15:41:32 +0700

    #7 add description to each route

--- back/routes/api.php
+++ back/routes/api.php
@@ -18,16 +18,17 @@ use App\Http\Controllers\UserController;
 
 
 // Leaves routes
-Route::get('/leaves', [LeaveController::class, 'index']);
-Route::get('/leaves_user', [LeaveController::class, 'getLeavesUser']);
-Route::get('/leaves/{leave}', [LeaveController::class, 'show']);
-Route::get('/leaves_user/{id}', [LeaveController::class, 'getLeaveUser']);
-Route::post('/leaves', [LeaveController::class, 'store']);
-Route::put('/leaves/status/{leave}', [LeaveController::class, 'updateStatus']);
-Route::put('/leaves/review/{leave}', [LeaveController::class, 'updateAsViewed']);
-Route::delete('/leaves/{leave}', [LeaveController::class, 'destroy']);
+Route::get('/leaves', [LeaveController::class, 'index']); /* The route to get all leaves */
+Route::get('/leaves_user', [LeaveController::class, 'getLeavesUser']); /* The route to get all leaves with belonged user */
+Route::get('/leaves/{leave}', [LeaveController::class, 'show']); /* The route to get one leave */
+Route::get('/leaves_user/{id}', [LeaveController::class, 'getLeaveUser']); /* The route to get one leave with belonged user */
+Route::post('/leaves', [LeaveController::class, 'store']); /* The route to create a new leave */
+Route::put('/leaves/status/{leave}', [LeaveController::class, 'updateStatus']); /* The route to update a leave’s status */
+Route::put('/leaves/review/{leave}', [LeaveController::class, 'updateAsViewed']); /* The route to update a leave as reviewed */
+Route::delete('/leaves/{leave}', [LeaveController::class, 'destroy']); /* The route to delete a leave */
 
-// Users routes
+
+// Users(students) routes
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users_leaves', [UserController::class, 'getUsersLeaves']);
 Route::get('/users/{user}', [UserController::class, 'show']);
