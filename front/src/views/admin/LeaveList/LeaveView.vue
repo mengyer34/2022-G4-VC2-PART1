920b6891ba4331e201211553b595537aca3700e9
SEYHA.SOEURN <SEYHA.SOEURN@pnc.lan>
2022-09-04 18:14:54 +0700

    #33 update count notifications for admin

--- front/src/views/admin/LeaveList/LeaveView.vue
+++ front/src/views/admin/LeaveList/LeaveView.vue
@@ -32,7 +32,7 @@
                 </div>
 
                 <!-- list all leaves -->
-                <student-leave @updateLeave="updateLeave" :isUpdating="isUpdating" @start-updating="isUpdating = true" :isGettingResources="isGettingResources" :leaves="filterLeaves" @get-leaves="getLeaves" @update-nav="$emit('update-nav')" />
+                <student-leave :isUpdating="isUpdating" @start-updating="isUpdating = true" :isGettingResources="isGettingResources" :leaves="filterLeaves" @get-leaves="getLeaves" @update-nav="$emit('update-nav')" />
 
                 <div v-if="leaves.length <= 0 && !isGettingResources" class="flex flex-col items-center mt-8 mb-3">
                     <img class="w-32" src="./../../../assets/request_empty.png" alt="Image not found">
@@ -99,14 +99,6 @@ export default {
 
         updateKeyword(keyword) {
             this.keyword = keyword;
-        },
-
-        updateLeave(id) {
-            this.leaves.forEach((eachLeave, index) => {
-                if (eachLeave.id == id) {
-                    this.leaves[index].is_admin_seen = true;
-                }
-            });
         },
 
         timer() {
